8a48dd1e121d0fa8a4f26b7e1071e05ab52f2b13
unknown <ahuffj1@wgu.edu>
2026-01-29 20:09:59 -0800

    set up lab environment

---
+++ src/main/java/com/elixirr/labRedo2/LabRedo2Application.java
@@ -0,0 +1,13 @@
+package com.elixirr.labRedo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRedo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRedo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/elixirr/labRedo2/LabRedo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixirr.labRedo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRedo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
